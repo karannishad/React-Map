c0d0b9948de4911a1e99563205b8ecc687f45d18
karannishad <karan.nishad3@gmail.com>
2019-03-01 15:08:32 +0530

    ads

--- src/components/weather.jsx
+++ src/components/weather.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import axios from "react-axios";
+import axios from "axios";
 class Weather extends Component {
   state = { hits: null };
   componentDidMount() {
